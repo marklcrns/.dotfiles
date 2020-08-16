3626ae1b8793eccd6e820858bb0708af82ffa2d2
Mark Lucernas <marklcrns@gmail.com>
2020-08-16 12:19:01 -0700

    Fixed stupid bug in git_clone()

--- scripts/install/install_utils.sh
+++ scripts/install/install_utils.sh
@@ -91,13 +91,13 @@ curl_install() {
 }
 
 git_clone() {
-  from="$1"
-  to="$1"
+  from=$1
+  to=$2
 
   # If output/destination file is given, else use regular curl
   if [[ -n "${to}" ]]; then
     # Check destination directory validity
-    if [[ ! -d "$(echo $(dirname "${to}"))" ]]; then
+    if [[ ! -d "$(dirname "${to}")" ]]; then
       error "Invalid git clone destination path '${to}'"
       return 1
     fi
