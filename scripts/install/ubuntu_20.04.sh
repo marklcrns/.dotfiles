3827dba7d0d447535751734dd25d5d6eae99bad5
Mark Lucernas <marklcrns@gmail.com>
2020-08-15 22:30:46 -0700

    Update ubuntu 20.04 script

--- scripts/install/ubuntu_20.04.sh
+++ scripts/install/ubuntu_20.04.sh
@@ -141,11 +141,8 @@ cat /etc/resolv.conf > ~/nameserver.txt
 echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
 
 # Apt update and upgrade
-if sudo apt update -y; then
-  error "Apt update failed" 1
-fi
-if sudo apt upgrade -y; then
-  error "Apt upgrade failed" 1
+if sudo apt update upgrade -y; then
+  error "Apt update and upgrade failed" 1
 fi
 
 echolog
