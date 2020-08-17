2a72ed0abd7210e07ed5433b905186dd31676c23
Mark Lucernas <marklcrns@gmail.com>
2020-08-16 18:27:45 -0700

    Update shell.sh

--- scripts/install/packages/shell.sh
+++ scripts/install/packages/shell.sh
@@ -12,6 +12,7 @@ apt_bulk_install "${APT_PACKAGES_SHELL[@]}"
 
 # Oh-my-zsh, plugins and themes
 if sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"; then
+  [[ ! -d "${HOME}/.oh-my-zsh/custom/plugins" ]] && mkdir -p "${HOME}/.oh-my-zsh/custom/plugins"
   git_clone "https://github.com/zsh-users/zsh-autosuggestions" "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
   git_clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
   git_clone "https://github.com/romkatv/powerlevel10k.git" "${HOME}/.oh-my-zsh/themes/powerlevel10k"
