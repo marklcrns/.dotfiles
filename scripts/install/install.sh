4913651ae6f4976772bf3880366089cb95fc23ae
Mark Lucernas <marklcrns@gmail.com>
2020-11-28 18:39:57 -0800

    feat(install): Added Raspbian distro
    - Added packages installation for Raspbian buster
    - Removed some apt application from packages

--- scripts/install/install.sh
+++ scripts/install/install.sh
@@ -10,4 +10,13 @@ if lsb_release -i | grep "Ubuntu" &> /dev/null; then
     echo "Installing packages for Ubuntu 20.04 Focal Fossa"
     ${SCRIPTDIR}/ubuntu_20.04.sh
   fi
+
+# Raspberry Pi Raspbian distributions
+elif lsb_release -i | grep "Raspbian" &> /dev/null; then
+  # Raspbian buster
+  if lsb_release -c | grep "buster" &> /dev/null; then
+    echo "Installing packages for Raspbian buster"
+    ${SCRIPTDIR}/raspbian_buster.sh
+  fi
+
 fi
