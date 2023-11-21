1073921346035686eb69a0594f46f4cc8a6e03e7
Mark Lucernas <marklcrns@gmail.com>
2023-11-20 23:12:52 -0800

    refactor(dots): remove unnecessary dots

--- mnt/c/Users/markl/wsl/start_servers.bat
+++
@@ -1,5 +0,0 @@
-:: Consolestate https://www.robvanderwoude.com/battech_hideconsole.php
-C:\wsl\ConsoleState /Hide
-:: start "" /B "C:\wsl\config.xlaunch"
-start /B x410.exe /wm /public
-start "" /B C:\wsl\pulse\pulseaudio.exe -F C:\wsl\pulse\config.pa
